34405f167b5067114c2502f7f88b332ecdfaa0c5
Domantas Jovaisas <domantasjovaisas@gmail.com>
2019-06-17 15:26:34 +0300

    Merge remote-tracking branch 'origin/DataBase' into DataBase
    # Conflicts:
    #	src/CodeMash.Core.Tests/DbTests.cs
    #	src/CodeMash.Repository/CodeMashRepository.cs
    #	src/CodeMash.Repository/CodeMashRepositoryFactory.cs
    #	src/TestApp/Program.cs

--- src/CodeMash.Core.Tests/Db/FindOneTests.cs
+++ src/CodeMash.Core.Tests/Db/FindOneTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 namespace CodeMash.Core.Tests
 {
     [TestFixture]
-    public class FindOneTests : DbTestBase
+    public class FindOneTests 
     {        
         [Test]
         [Category("Db")]
@@ -14,11 +14,11 @@ namespace CodeMash.Core.Tests
         [Category("FindOne")]
         public void Can_find_one_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var train = recipesRepository.FindOne<Train>(x => x.Destination == "Trakai");
+            var train = recipesRepository.FindOne<Schedule>(x => x.Destination == "Trakai");
             
-            Assert.IsInstanceOf<Train>(train);
+            Assert.IsInstanceOf<Schedule>(train);
             Assert.IsNotNull(train);
             Assert.AreEqual(train.Destination, "Trakai");
         }
@@ -29,13 +29,13 @@ namespace CodeMash.Core.Tests
         [Category("FindOne")]
         public void Can_find_one_value_in_number_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var train = recipesRepository.FindOne<Train>(x => x.Number == "1454");
+            var train = recipesRepository.FindOne<Schedule>(x => x.Number == 1454);
             
-            Assert.IsInstanceOf<Train>(train);
+            Assert.IsInstanceOf<Schedule>(train);
             Assert.IsNotNull(train);
-            Assert.AreEqual(train.Id, "5d0236f623ef8d00013fb462");
+            Assert.AreEqual(train.Id, "5d0782db33ab560001f28fb8");
         }
 
         [Test]
@@ -44,9 +44,9 @@ namespace CodeMash.Core.Tests
         [Category("FindOneOne")]
         public void Exception_find_one_with_no_filter_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            Assert.Throws<ArgumentNullException>( () => recipesRepository.FindOne<Train>(null) );
+            Assert.Throws<ArgumentNullException>( () => recipesRepository.FindOne<Schedule>(null) );
         }
 
         [Test]
@@ -55,11 +55,12 @@ namespace CodeMash.Core.Tests
         [Category("FindOne")]
         public void Can_find_one_with_id_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var train = recipesRepository.FindOne<Train>(x => x.Id == "5d0236ba23ef8d00013fb409");
+            // TODO : change it later. Either we need setup tests and seed with data first or Insert and get data upfront of each test execution.
+            var train = recipesRepository.FindOne<Schedule>(x => x.Id == "5d07829733ab560001f28f91");
             
-            Assert.IsInstanceOf<Train>(train);
+            Assert.IsInstanceOf<Schedule>(train);
             Assert.IsNotNull(train);
             Assert.AreEqual(train.Origin, "Kaunas");
             Assert.AreEqual(train.Destination, "Vilnius");
@@ -71,11 +72,11 @@ namespace CodeMash.Core.Tests
         [Category("FindOne")]
         public void Can_find_one_first_with_origin_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var train = recipesRepository.FindOne<Train>(x => x.Origin == "Kaunas");
+            var train = recipesRepository.FindOne<Schedule>(x => x.Origin == "Kaunas");
             
-            Assert.IsInstanceOf<Train>(train);
+            Assert.IsInstanceOf<Schedule>(train);
             Assert.IsNotNull(train);
             Assert.AreEqual(train.Origin, "Kaunas");
             Assert.AreEqual(train.Destination, "Vilnius");
--- src/CodeMash.Core.Tests/Db/FindTests.cs
+++ src/CodeMash.Core.Tests/Db/FindTests.cs
@@ -6,19 +6,28 @@ using NUnit.Framework;
 namespace CodeMash.Core.Tests
 {
     [TestFixture]
-    public class FindTests : DbTestBase
-    {        
+    public class FindTests
+    {
+        // TODO : add setup which runs before each test
+        // TODO : add all possible fields (Selections, Taxonomies, Files, Translatable fields)
+        // TODO : play with projections
+        // TODO : play with paging and sorting
+        // TODO : play with cultures and translatable fields. 
+        // 
+        
+        
+        
         [Test]
         [Category("Db")]
         [Category("Integration")]
         [Category("Find")]
         public void Can_find_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(x => true);
+            var trains = recipesRepository.Find<Schedule>(x => true);
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(4, trains.Count);
         }
@@ -29,11 +38,11 @@ namespace CodeMash.Core.Tests
         [Category("Find")]
         public void Can_find_value_in_origin_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(x => x.Origin == "Kaunas");
+            var trains = recipesRepository.Find<Schedule>(x => x.Origin == "Kaunas");
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(2, trains.Count);
         }
@@ -44,11 +53,11 @@ namespace CodeMash.Core.Tests
         [Category("Find")]
         public void Can_find_value_in_origin_and_destination_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(x => x.Origin == "Kaunas" || x.Destination == "Kaunas");
+            var trains = recipesRepository.Find<Schedule>(x => x.Origin == "Kaunas" || x.Destination == "Kaunas");
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(3, trains.Count);
         }
@@ -59,11 +68,11 @@ namespace CodeMash.Core.Tests
         [Category("Find")]
         public void Can_find_with_no_filter_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(null);
+            var trains = recipesRepository.Find<Schedule>(null);
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(4, trains.Count);
         }
@@ -74,11 +83,11 @@ namespace CodeMash.Core.Tests
         [Category("Find")]
         public void Can_find_with_id_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(x => x.Id == "5d0236ba23ef8d00013fb409");
+            var trains = recipesRepository.Find<Schedule>(x => x.Id == "5d07829733ab560001f28f91");
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(trains[0].Origin, "Kaunas");
             Assert.AreEqual(trains[0].Destination, "Vilnius");
@@ -90,11 +99,11 @@ namespace CodeMash.Core.Tests
         [Category("Find")]
         public void Can_find_with_limit_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(x => true, null, null, 2, null);
+            var trains = recipesRepository.Find<Schedule>(x => true, null, null, 2, null);
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(2, trains.Count);
         }
@@ -105,11 +114,11 @@ namespace CodeMash.Core.Tests
         [Category("Find")]
         public void Can_find_with_limit_2_and_skip_1_page_integration_test()
         {
-            var recipesRepository = CodeMashRepositoryFactory.Create<Train>("appsettings.Production.json");
+            var recipesRepository = CodeMashRepositoryFactory.Create<Schedule>("appsettings.Production.json");
 
-            var trains = recipesRepository.Find<Train>(x => true, null, 1, 2, null);
+            var trains = recipesRepository.Find<Schedule>(x => true, null, 1, 2, null);
             
-            Assert.IsInstanceOf<List<Train>>(trains);
+            Assert.IsInstanceOf<List<Schedule>>(trains);
             Assert.IsNotNull(trains);
             Assert.AreEqual(2, trains.Count);
             Assert.True(Convert.ToInt32(trains[0].Number) > 200);
