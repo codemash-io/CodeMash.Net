e7f866ed2eff5893c8b10787d053376951589933
CodeMash <domantas@isidos.lt>
2021-05-13 00:15:53 +0300

    lets push

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WORKING_DIR=$1
+SRC_DIR=$WORKING_DIR/src
+PKG_DIR=$WORKING_DIR/packages
+
+mkdir -p $PKG_DIR
+rm -rf $PKG_DIR/*.nupkg
+
+dotnet pack --configuration=release $SRC_DIR/CodeMash.Sdk.sln -p:Version=2.1.0 -p:NuspecFile=../../Nuget.Core/CodeMash.Core.nuspec  
+
+# dotnet build --configuration=release $SRC_DIR/CodeMash.Sdk.sln
+cp $SRC_DIR/**/bin/release/*.nupkg $PKG_DIR
